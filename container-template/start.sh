ec7a64e9317a9dc287d67c09f461f285801be4d4
mchekm <mchekm@softserveinc.com>
2026-05-22 13:49:56 +0300

    fix: Sonar findings

--- container-template/start.sh
+++ container-template/start.sh
@@ -29,25 +29,25 @@ setup_ssh() {
         echo "$PUBLIC_KEY" >> ~/.ssh/authorized_keys
         chmod 700 -R ~/.ssh
 
-        if [ ! -f /etc/ssh/ssh_host_rsa_key ]; then
+        if [[ ! -f /etc/ssh/ssh_host_rsa_key ]]; then
             ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -q -N ''
             echo "RSA key fingerprint:"
             ssh-keygen -lf /etc/ssh/ssh_host_rsa_key.pub
         fi
 
-        if [ ! -f /etc/ssh/ssh_host_dsa_key ]; then
+        if [[ ! -f /etc/ssh/ssh_host_dsa_key ]]; then
             ssh-keygen -t dsa -f /etc/ssh/ssh_host_dsa_key -q -N ''
             echo "DSA key fingerprint:"
             ssh-keygen -lf /etc/ssh/ssh_host_dsa_key.pub
         fi
 
-        if [ ! -f /etc/ssh/ssh_host_ecdsa_key ]; then
+        if [[ ! -f /etc/ssh/ssh_host_ecdsa_key ]]; then
             ssh-keygen -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key -q -N ''
             echo "ECDSA key fingerprint:"
             ssh-keygen -lf /etc/ssh/ssh_host_ecdsa_key.pub
         fi
 
-        if [ ! -f /etc/ssh/ssh_host_ed25519_key ]; then
+        if [[ ! -f /etc/ssh/ssh_host_ed25519_key ]]; then
             ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -q -N ''
             echo "ED25519 key fingerprint:"
             ssh-keygen -lf /etc/ssh/ssh_host_ed25519_key.pub
